7eb00cce5c5cff53c79000beaf14d41979919350
Ida Li <ida.li@nrm.se>
2019-08-09 16:43:32 +0200

    Fix paging navitation controller

--- dina-web-portal/src/main/java/se/nrm/dina/web/portal/controller/PagingNavigation.java
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/controller/PagingNavigation.java
@@ -8,10 +8,10 @@ package se.nrm.dina.web.portal.controller;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import lombok.extern.slf4j.Slf4j;
-import se.nrm.dina.web.portal.model.SolrResult;
+import lombok.extern.slf4j.Slf4j; 
 
 /**
  *
@@ -24,256 +24,141 @@ public class PagingNavigation implements Serializable {
 
   private int currentPage = 1;
   private int totalPages = 1;
+  private int startPage = 1;
+  private int endPage = 10;
 
   private int totalFound = 0;
   private int start = 0;
   private int end = 0;
-
-  private int resultViewStart = 1;
-
-  private List<Integer> pages = new ArrayList<>();
-  private boolean previousDisable = false;
-  private boolean nextDisable = false;
+  
+  private final List<Integer> pages = new ArrayList<>();
 
   public PagingNavigation() {
-
   }
 
-  public void resetdata() {
+  public void calculateTotalPages(int totalFound, int numPerPage) {
+    log.info("calculateTotalPages : {}", numPerPage);
+
+    this.totalFound = totalFound;
+    start = 1;
+    end = numPerPage;
+    startPage = 1;
     currentPage = 1;
-    totalFound = 0;
-    totalPages = 1;
-    start = 0;
-    end = 0;
-    previousDisable = false;
-    nextDisable = false;
+    if (totalFound > numPerPage) {
+      totalPages = totalFound / numPerPage;
+      if (totalFound % numPerPage > 0) {
+        totalPages++;
+      }
+    }
+    endPage = totalPages < 10 ? totalPages : 10; 
+    setPageNumber();
+  }
+   
+  public void setNextPage(int numPerPage) {
+    log.info("num per page : {}", numPerPage);
+    
+    start += numPerPage;
+    currentPage++; 
+    end = currentPage < totalPages ? end + numPerPage : totalFound; 
+    setNextPageRange();
   }
 
-  public List<Integer> getPages() {
-    return pages;
+  public void setPreviousPage(int numPerPage) {
+    log.info("setPreviousPage: {}", numPerPage);
+
+    start -= numPerPage;
+    end = currentPage < totalPages ? end - numPerPage : start + numPerPage - 1;
+    currentPage--;
+    setPreviousPageRange();
   }
 
-  public void setPages(List<Integer> pages) {
-    this.pages = pages;
+  public void setFirstPage(int numPerPage) {
+    start = 1;
+    end = numPerPage;
+    currentPage = 1;
+    startPage = 1; 
+     
+    endPage = totalPages < 10 ? totalPages : 10; 
+    setPageNumber(); 
   }
 
+  public void setLastPage(int numDisplay) { 
+    start = numDisplay * (totalPages - 1) + 1;
+    end = totalFound;
+    endPage = totalPages;
+    startPage = totalPages - 9;
+    currentPage = totalPages;
+    setPageNumber();
+  }
+  
+  public void setPaging(int start, int numPerPage, int currentPage) { 
+    this.start = start + 1;
+    end = start + numPerPage;
+    this.currentPage = currentPage;
+    setPageRange();
+  }
+ 
+  private void setPageNumber() {
+    log.info("setPageNumber: {} -- {}", startPage, endPage);
+    
+    pages.clear();
+    IntStream.rangeClosed(startPage, endPage).forEach(i -> {
+      pages.add(i);
+    });
+  }
+  
+  private void setPageRange() {
+    if(currentPage > 6 || totalPages - currentPage < 5) { 
+      startPage = currentPage - 5;
+      endPage = currentPage + 4;
+    }
+    setPageNumber();
+  }
+  
+  private void setNextPageRange() {
+
+    int middlePage = (endPage - startPage) / 2 + startPage; 
+    if (currentPage > middlePage) {
+      if (endPage < totalPages) {
+        startPage++;
+        endPage++;
+      }
+    }
+    setPageNumber();
+  }
+
+  private void setPreviousPageRange() {
+    int middlePage = (endPage - startPage) / 2 + startPage;
+    if (currentPage < middlePage + 1) {
+      if (startPage > 1) {
+        startPage--;
+        endPage--;
+      }
+    }
+    setPageNumber();
+  }
+  
   public int getStart() {
     return start;
   }
 
-  public void setStart(int start) {
-    this.start = start;
-  }
-
   public int getEnd() {
     return end;
-  }
-
-  public void setEnd(int end) {
-    this.end = end;
-  }
-
-  public int getTotalFound() {
-    return totalFound;
-  }
-
-  public void setTotalFound(int totalFound) {
-    this.totalFound = totalFound;
-  }
-
-  public int getCurrentPage() {
-    return currentPage;
-  }
-
-  public void setCurrentPage(int currentPage) {
-    this.currentPage = currentPage;
-  }
-
-  public boolean isNextDisable() {
-    return nextDisable;
-  }
-
-  public void setNextDisable(boolean nextDisable) {
-    this.nextDisable = nextDisable;
-  }
-
-  public boolean isPreviousDisable() {
-    return previousDisable;
-  }
-
-  public void setPreviousDisable(boolean previousDisable) {
-    this.previousDisable = previousDisable;
-  }
-
+  } 
+  
   public int getTotalPages() {
     return totalPages;
   }
-
-  public void setTotalPages(int totalPages) {
-    this.totalPages = totalPages;
+  
+  public int getTotalFound() {
+    return totalFound;
   }
-
-  public int getResultViewStart() {
-    return resultViewStart;
+  
+  public List<Integer> getPages() {
+    return pages;
   }
-
-  public void setResultViewStart(int resultViewStart) {
-    this.resultViewStart = resultViewStart;
-  }
-
-  public void resetCurrentPage(SolrResult result, int numPerPage) {
-    start = result.getStart();
-    if (start == 0) {
-      currentPage = 1;
-    } else {
-      currentPage = (start + numPerPage) / numPerPage;
-    }
-  }
-
-  public int resetPage(SolrResult result, int numPerPage, int total) {
- 
-
-    totalFound = total;
-
-    if (result != null) {
-      start = result.getStart();
-      if (start == 0) {
-        currentPage = 1;
-      }
-
-      end = start + numPerPage;
-      if (end > totalFound) {
-        end = totalFound;
-      }
-      setNavigationButtons(numPerPage);
-      resultViewStart = start + 1;
-    } else {
-      totalFound = 0;
-      pages = new ArrayList<>();
-      currentPage = 0;
-      start = 0;
-      end = 0;
-      resultViewStart = 0;
-    }
+  
+  public int getCurrentPage() {
     return currentPage;
-  }
-
-  public void setPreviousPage(int numDisplay) {
-
-    if (currentPage > 1) {
-      start -= numDisplay;
-      currentPage--;
-    }
-  }
-
-  public void setNextPage(int numDisplay) { 
-    log.info("num per page : {}", numDisplay);
-    if (currentPage < totalPages) {
-      start += numDisplay;
-      currentPage++;
-    }
-  }
-
-  public void setFirstPage() {
-    start = 0;
-    setCurrentPage(1);
-  }
-
-  public void setLastPage(int numDisplay) {
-
-    start = numDisplay * (totalPages - 1);
-    setCurrentPage(totalPages);
-  }
-
-  public void setNavigationButtons(int numPerPage) {
-
-    log.info("setNavigationButtons : {}", numPerPage);
-
-    boolean onePage = true;
-
-    pages = new ArrayList<>();
-    setPreviousDisable(false);
-    setNextDisable(false);
-
-    if (totalFound > numPerPage) {
-      totalPages = totalFound / numPerPage;
-
-      if (totalPages > 0) {
-        if (totalFound % numPerPage > 0) {
-          totalPages++;
-        }
-      }
-      onePage = false;
-
-      if (totalPages > 10) {
-        setPageNum(totalPages);
-      } else {
-        for (int i = 1; i <= totalPages; i++) {
-          pages.add(i);
-        }
-      }
-    } else {
-      if (totalFound > 0) {
-        setCurrentPage(1);
-        setTotalPages(1);
-        pages = new ArrayList<>();
-        pages.add(1);
-      }
-    }
-    setPageNavigationButtons(onePage);
-  }
-
-  private void setPageNavigationButtons(boolean onePage) {
-
-    log.info("currentpage : {} -- start {}", currentPage, start);
-
-    if (onePage) {
-      setPreviousDisable(true);
-      setNextDisable(true);
-    } else {
-      if (currentPage == 1) {
-        setPreviousDisable(true);
-      } else if (currentPage == totalPages) {
-        setNextDisable(true);
-      }
-    }
-  }
-
-  private void setPageNum(int totalPages) {
-
-    if (currentPage <= 6) {
-      pages = new ArrayList<>();
-      for (int i = 1; i <= 10; i++) {
-        pages.add(i);
-      }
-    } else {
-      if (totalPages - currentPage > 4) {
-        for (int i = 5; i >= 0; i--) {
-          int count = currentPage - i;
-          pages.add(count);
-        }
-
-        if (currentPage < totalPages) {
-          int count = currentPage;
-          while (count < totalPages && pages.size() < 10) {
-            count++;
-            pages.add(count);
-          }
-        }
-      } else {
-        int remainder = totalPages - 9;
-        for (int i = remainder; i <= currentPage; i++) {
-          pages.add(i);
-        }
-
-        if (currentPage <= totalPages) {
-          int count = currentPage;
-          while (count < totalPages && pages.size() < 10) {
-            count++;
-            pages.add(count);
-          }
-        }
-      }
-    }
-  }
+  } 
 }
