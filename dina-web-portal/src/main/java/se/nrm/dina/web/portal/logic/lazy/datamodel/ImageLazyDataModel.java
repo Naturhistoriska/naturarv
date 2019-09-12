f0b742c118a834281aeac135009b1a071218a79a
Ida Li <ida.li@nrm.se>
2019-09-12 08:41:21 +0200

    Refactor ImageLazyDataModel

--- dina-web-portal/src/main/java/se/nrm/dina/web/portal/logic/lazy/datamodel/ImageLazyDataModel.java
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/logic/lazy/datamodel/ImageLazyDataModel.java
@@ -4,11 +4,10 @@
  * and open the template in the editor.
  */
 package se.nrm.dina.web.portal.logic.lazy.datamodel;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map; 
-import static java.util.stream.Collectors.toList;
+import java.util.Map;  
 import lombok.extern.slf4j.Slf4j;
 import org.primefaces.model.LazyDataModel; 
 import org.primefaces.model.SortOrder; 
@@ -24,30 +23,49 @@ public class ImageLazyDataModel extends LazyDataModel<ImageModel> {
  
   private final SolrService solr;
   
-  private final StringBuilder searchText;
+  private final String searchText;
   private final Map<String, String> filterMap;
   private final List<String> filterList;
+  private List<ImageModel> datasource;
 
   public ImageLazyDataModel(SolrService solr, Map<String, String> filterMap, 
-                            List<String> filterList, StringBuilder searchText) { 
+                            List<String> filterList, String searchText, int totalCount) { 
     log.info("ImageLazyDataModel: {} -- {}", filterMap, filterList + " -- " + searchText);
+    datasource = new ArrayList<>();
     this.solr = solr;
     this.searchText = searchText;
     this.filterMap = filterMap;
     this.filterList = filterList;
-    this.setRowCount(solr.getImageTotalCount(searchText.toString(), filterMap));
+    this.setRowCount(totalCount);
   }
 
+  
+//  @Override
+//  public ImageModel getRowData(String rowKey) {
+//        for(ImageModel model : datasource) {
+//            if(model.getImageId().equals(rowKey))
+//                return model;
+//        }
+// 
+//        return null;
+//    }
+// 
+//    @Override
+//    public Object getRowKey(ImageModel model) {
+//        return model.getImageId();
+//    }
+ 
+  
   @Override
   public List<ImageModel> load(int first, int pageSize, String sortField, 
           SortOrder sortOrder, Map<String, Object> filters) {
-    log.info("filterMap: {} -- {}", filterList, searchText.toString());
+    log.info("filterMap: {} -- {}", filterList, searchText + " --- " + filterMap );
     
-    List<ImageModel> data = solr.getImageList(searchText.toString(), first, pageSize, filterMap, filterList);
-
-    if(filterList != null && !filterList.isEmpty()) {
-      this.setRowCount(solr.getImageTotalCount(searchText.toString(), filterMap));
+    List<ImageModel> data = solr.getImageList(searchText, first, pageSize, filterMap, filterList);
+    datasource = data;
+    if(filterList != null && !filterList.isEmpty() || filterMap != null && !filterMap.isEmpty()) {
+      this.setRowCount(solr.getImageTotalCount(searchText, filterMap, filterList));
     } 
-    return data;
+    return datasource;
   }
 }
