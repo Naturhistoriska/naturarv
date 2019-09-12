fe725e6546bd05d7b23290f4ee0c78455b745158
Ida Li <ida.li@nrm.se>
2019-09-12 08:40:35 +0200

    Add SingleMap for displaying map in result page

---
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/controller/SingleMap.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.nrm.dina.web.portal.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import lombok.extern.slf4j.Slf4j;
+import org.primefaces.event.map.OverlaySelectEvent;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+import se.nrm.dina.web.portal.model.SolrData;
+
+/**
+ *
+ * @author idali
+ */
+@Named("map")
+@SessionScoped
+@Slf4j
+public class SingleMap implements Serializable {
+
+  private static final String GMAP_MARKER_PATH = "http://maps.google.com/mapfiles/ms/micons/red-dot.png";
+  private MapModel singleModal;
+
+  public SingleMap() {
+    singleModal = new DefaultMapModel();
+  }
+
+  public MapModel getSingledModel(SolrData data) {
+
+    log.info("getSingledModel: {}", data);
+
+    singleModal = new DefaultMapModel();
+    if (data != null) {
+      singleModal.addOverlay(new Marker(data.getLatLng(), data.getLocality(), null, GMAP_MARKER_PATH));
+    }
+    return singleModal;
+  }
+  
+  public void onSingleMarkerSelect(OverlaySelectEvent event) {
+    log.info("onSingleMarkerSelect");
+  } 
+}
