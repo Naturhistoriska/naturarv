457b032aa6869d8b4276fcfe6eab77375a0740c0
Ida Li <ida.li@nrm.se>
2019-09-12 08:43:46 +0200

    Refactor SolrHelper

--- dina-web-portal/src/main/java/se/nrm/dina/web/portal/solr/SolrHelper.java
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/solr/SolrHelper.java
@@ -5,6 +5,10 @@
  */
 package se.nrm.dina.web.portal.solr;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.function.Predicate;
 import org.apache.commons.lang3.StringUtils;
 import se.nrm.dina.web.portal.utils.CommonText;
 
@@ -13,8 +17,11 @@ import se.nrm.dina.web.portal.utils.CommonText;
  * @author idali
  */
 public class SolrHelper {
-  
+
   private static SolrHelper instance = null;
+  private StringBuilder searchCatalogedDateSb;
+  private StringBuilder searchTextSb;
+  private StringJoiner sj;
 
   public static synchronized SolrHelper getInstance() {
     if (instance == null) {
@@ -22,33 +29,67 @@ public class SolrHelper {
     }
     return instance;
   }
-  
+
   public String buildSearchText(String field, String text) {
-    StringBuilder sb = new StringBuilder();
-    
-    if(text == null || text.isEmpty()) {
+    searchTextSb = new StringBuilder();
+
+    if (text == null || text.isEmpty()) {
       return CommonText.getInstance().getWildSearchText();
     }
-    
+
     String[] strings = StringUtils.split(text);
-    if(strings.length == 1) {
-      sb.append(field);
-      sb.append(":*");
-      sb.append(text);
-      sb.append("*");
+    if (strings.length == 1) {
+      searchTextSb.append(field);
+      searchTextSb.append(":*");
+      searchTextSb.append(text);
+      searchTextSb.append("*");
     } else {
-      sb.append("(");
-      for(String s : strings) {
-        if(!s.trim().isEmpty()) {
-          sb.append("+");
-          sb.append(field);
-          sb.append(":*");
-          sb.append(text);
-          sb.append("*");
-        } 
+      searchTextSb.append("(");
+      for (String s : strings) {
+        if (!s.trim().isEmpty()) {
+          searchTextSb.append("+");
+          searchTextSb.append(field);
+          searchTextSb.append(":*");
+          searchTextSb.append(text);
+          searchTextSb.append("*");
+        }
       }
-      sb.append(")");
+      searchTextSb.append(")");
     }
-    return sb.toString();
+    return searchTextSb.toString();
   }
+
+  public String buildSearchCatalogedDateText(LocalDateTime date) {
+    searchCatalogedDateSb = new StringBuilder();
+    searchCatalogedDateSb.append("catalogedDate:[");
+    searchCatalogedDateSb.append(date);
+    searchCatalogedDateSb.append(":00Z TO *]");
+    return searchCatalogedDateSb.toString();
+  }
+
+  public String buildImageOptionSearchText(String searchText, List<String> filters) { 
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("+(");
+    sb.append(searchText);
+    sb.append(") "); 
+    buildString(sb, filters); 
+    return sb.toString(); 
+  }
+
+  private void buildString(StringBuilder sb, List<String> selectedList) {
+    if (!selectedList.isEmpty()) {
+      sb.append(" +(");
+      sb.append(CommonText.getInstance().getImageViewKey());
+      sb.append(" (*");
+      sj = new StringJoiner("* *");
+
+      selectedList.stream() 
+              .forEach(v -> {
+                sj.add(v);
+              });
+      sb.append(sj.toString());
+      sb.append("*)) ");
+    }
+  } 
 }
