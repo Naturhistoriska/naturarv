49974222be1e5a9453aaf40474f7fa32b336a0dc
Ida Li <ida.li@nrm.se>
2019-09-12 08:35:42 +0200

    Add common text

--- dina-web-portal/src/main/java/se/nrm/dina/web/portal/utils/CommonText.java
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/utils/CommonText.java
@@ -35,19 +35,60 @@ public class CommonText {
 
   private static final String CHART_YAXIS_SV = "Antal föremål";
   private static final String CHART_YAXIS_EN = "Total specimens";
-  
+
+  private static final String ERROR_REPORT_TITLE_EN = "Error Report";
+  private static final String ERROR_REPORT_TITLE_SV = "Felrapport";
+
+  private static final String TOTAL_EN = "Total";
+  private static final String TOTAL_SV = "Totalt";
+
+  private static final String REG_NO_EN = "Reg. no.: ";
+  private static final String REG_NO_SV = "Reg. nr.: ";
+  private static final String SPECIES_NAME_SV = "Artnamn: ";
+  private static final String SPECIES_NAME_EN = "Species name: ";
+  private static final String TYPE_INFORMATION_SV = "Typinformation: ";
+  private static final String TYPE_INFORMATION_EN = "Type information: ";
+  private static final String FAMILY_EN = "Family: ";
+  private static final String FAMILY_SV = "Familj: ";
+  private static final String COLLECTORS_SV = "Insamlare: ";
+  private static final String COLLECTORS_EN = "Collectors: ";
+  private static final String DATE_SV = "Datum: ";
+  private static final String DATE_EN = "Date: ";
+  private static final String LOCALITY_SV = "Lokal: ";
+  private static final String LOCALITY_EN = "Locality: ";
+  private static final String CONTINENT_EN = "Continent: ";
+  private static final String CONTINENT_SV = "Kontinent: ";
+  private static final String COUNTRY_EN = "Country: ";
+  private static final String COUNTRY_SV = "Land: ";
+  private static final String COORDINATE_SV = "Koordinat: ";
+  private static final String COORDINATE_EN = "Coordinate: ";
+  private static final String OTHER_INFORMATION_SV = "&Ouml;vrig etikettinfo: ";
+  private static final String OTHER_INFORMATION_EN = "Other information: ";
+  private static final String DETERMINER_SV = "Best&auml;mningar: ";
+  private static final String DETERMINER_EN = "Determiner: ";
+  private static final String DESCRIPTION_SV = "Beskrivning: ";
+  private static final String DESCRIPTION_EN = "Description: ";
+  private static final String REPORT_BY_SV = "Rapporterat av: ";
+  private static final String REPORT_BY_EN = "Report by: ";
+
+  private static final String HITS_EN = "hits";
+  private static final String HITS_SV = "träffar";
+  private static final String SELECTED_EN = "Selected";
+  private static final String SELECTED_SV = "Valda";
+
   private static final String LOCALE = "locale";
   private static final String STATISTIC = "statistic";
   private static final String MONTH_CHART_DATA = "monthChartData";
   private static final String YEAR_CHART_DATA = "yearChartData";
   private static final String COLLECTIONS_MONTH_CHART_DATA = "collectionsMonthChartData";
   private static final String COLLECTIONS_YEAR_CHART_DATA = "collectionsYearChartData";
-  
+  private static final String CATALOGED_MONTH = "catalogedMonth";
+  private static final String CATALOGED_MONTH_STRING = "catalogedMonthString";
+
   private static final String SORT_BY_SCORE = "score";
-  
+
   private static final String WILD_SEARCH_TEXT = "*:*";
   private static final String WILD_CARD = "*";
-  private static final String MAP_KEY = "map:";
   private static final String IMAGE_KEY = "image:";
   private static final String TYPE_KEY = "isType:";
   private static final String DNA_KEY = "dna:";
@@ -55,17 +96,45 @@ public class CommonText {
   private static final String COLLECTION_CODE_KEY = "collectionId:";
   private static final String COLLECTION_NAME_KEY = "collectionName:";
   private static final String ID_KEY = "id:";
-  
+  private static final String GEO_KEY = "geo:";
+  private static final String MAP_KEY = "map:";
+  private static final String COORDINATE_KEY = "coordinate:";
+
   private static final String IMAGE_VIEW_KEY = "morphBankView:";
+  private static final String MORPHBANK_ID_KEY = "morphbankId:";
   private static final String TEXT_KEY = "text";
-  private static final String GEOPOINT_KEY = "geopoint";
-  
+  private static final String GEOPOINT = "geopoint";
+
   private static final String ID = "id";
   private static final String COLLECTION_NAME = "collectionName";
   private static final String COLLECTION_ID = "collectionId";
   private static final String CATALOGED_YEAR = "catalogedYear";
   private static final String TAXON_FULL_NAME = "txFullName";
   private static final String LOCATIONS = "locations";
+  private static final String COORDINATE = "coordinate";
+  private static final String CATALOG_NUMBER = "catalogNumber";
+  private static final String IMAGE_VIEW = "morphBankView";
+  private static final String IMAGE_ID = "morphbankImageId";
+  private static final String MORPHBANK_ID = "morphbankId";
+  
+  private static final String ALL = "all";
+
+  private static final String COLOR_1 = "coloer1";
+  private static final String COLOR_2 = "coloer2";
+  private static final String COLOR_3 = "coloer3";
+  private static final String COLOR_4 = "coloer4";
+  private static final String COLOR_5 = "coloer5";
+  private static final String COLOR_6 = "coloer6";
+
+  private static final String GROUP = "group";
+  private static final String GROUP_FIELD = "group.field";
+  private static final String GROUPS = "groups";
+  private static final String GROUPED = "grouped";
+  private static final String MATCHES = "matches";
+  
+  private static final String IMAGE_TYPE_THUMB = "&imgType=thumb";
+  private static final String IMAGE_QUERY_ID = "?id=";
+  private static final String IMAGE_TYPE_JPG = "&imgType=jpg";
 
   private static CommonText instance = null;
 
@@ -75,103 +144,183 @@ public class CommonText {
     }
     return instance;
   }
-  
+
+  public String getColor1() {
+    return COLOR_1;
+  }
+
+  public String getColor2() {
+    return COLOR_2;
+  }
+
+  public String getColor3() {
+    return COLOR_3;
+  }
+
+  public String getColor4() {
+    return COLOR_4;
+  }
+
+  public String getColor5() {
+    return COLOR_5;
+  }
+
+  public String getColor6() {
+    return COLOR_6;
+  }
+
+  public String getGroup() {
+    return GROUP;
+  }
+
+  public String getGroupField() {
+    return GROUP_FIELD;
+  }
+
+  public String getGroups() {
+    return GROUPS;
+  }
+
+  public String getGrouped() {
+    return GROUPED;
+  }
+
+  public String getMatches() {
+    return MATCHES;
+  }
+
   public String getId() {
     return ID;
   }
+
+  public String getCoordinate() {
+    return COORDINATE;
+  }
   
+  public String getCatalogNumber() {
+    return CATALOG_NUMBER;
+  }
+
   public String getTaxonFullName() {
     return TAXON_FULL_NAME;
   }
-  
+
   public String getCollectionId() {
     return COLLECTION_ID;
   }
-  
+
   public String getLocations() {
     return LOCATIONS;
   }
-  
+
   public String getCatalogedYear() {
     return CATALOGED_YEAR;
   }
   
+  public String getMorphbankId() {
+    return MORPHBANK_ID;
+  }
+  
+  public String getMorphbankIdKey() {
+    return MORPHBANK_ID_KEY;
+  }
+  
+  public String getImageView() {
+    return IMAGE_VIEW;
+  }
+  
+  public String getImageId() {
+    return IMAGE_ID;
+  }
+  
+  public String getAll() {
+    return ALL;
+  }
+
   public String getNrmCode() {
     return NRM_CODE;
   }
-  
+
   public String getGnmCode() {
     return GNM_CODE;
   }
-  
+
   public String getSortByScore() {
     return SORT_BY_SCORE;
   }
-  
+
   public String getWildCard() {
     return WILD_CARD;
   }
-  
+
   public String getIdKey() {
     return ID_KEY;
   }
-  
-  public String getCollectionNameKey() {
-    return COLLECTION_NAME_KEY;
-  }
-  
-  public String getCollectionCodeKey() {
-    return COLLECTION_CODE_KEY;
-  }
-  
-  public String getTextKey() {
-    return TEXT_KEY;
-  }
-  
-  public String getGeopointKey() {
-    return GEOPOINT_KEY;
-  }
-  
-  public String getCollectionName() {
-    return COLLECTION_NAME;
-  }
-  
-  public String getImageViewKey() {
-    return IMAGE_VIEW_KEY;
+
+  public String getGeoKey() {
+    return GEO_KEY;
   }
 
   public String getMapKey() {
     return MAP_KEY;
   }
-  
+
+  public String getCollectionNameKey() {
+    return COLLECTION_NAME_KEY;
+  }
+
+  public String getCollectionCodeKey() {
+    return COLLECTION_CODE_KEY;
+  }
+
+  public String getTextKey() {
+    return TEXT_KEY;
+  }
+
+  public String getCoordinateKey() {
+    return COORDINATE_KEY;
+  }
+
+  public String getGeopoint() {
+    return GEOPOINT;
+  }
+
+  public String getCollectionName() {
+    return COLLECTION_NAME;
+  }
+
+  public String getImageViewKey() {
+    return IMAGE_VIEW_KEY;
+  }
+
   public String getImageKey() {
     return IMAGE_KEY;
   }
-  
+
   public String getDNAKey() {
     return DNA_KEY;
   }
-  
+
   public String getTypeKey() {
     return TYPE_KEY;
   }
-  
+
   public String getSwedenKey() {
     return SWEDEN_KEY;
   }
-  
+
   public String getWildSearchText() {
     return WILD_SEARCH_TEXT;
   }
-  
+
   public String getLocale() {
     return LOCALE;
   }
-  
+
   public String getStatistic() {
     return STATISTIC;
   }
-  
+
   public String getMonthChartData() {
     return MONTH_CHART_DATA;
   }
@@ -179,15 +328,35 @@ public class CommonText {
   public String getYearChartData() {
     return YEAR_CHART_DATA;
   }
-  
+
   public String getCollectionsMonthChart() {
     return COLLECTIONS_MONTH_CHART_DATA;
   }
-  
+
   public String getCollectionsYearChart() {
     return COLLECTIONS_YEAR_CHART_DATA;
   }
+
+  public String getCatalogedMonth() {
+    return CATALOGED_MONTH;
+  }
+
+  public String getCatalogedMonthString() {
+    return CATALOGED_MONTH_STRING;
+  }
   
+  public String getImageTypeThumb() {
+    return IMAGE_TYPE_THUMB;
+  }
+  
+  public String getImageTypeJpg() {
+    return IMAGE_TYPE_JPG;
+  }
+  
+  public String getImageQueryId() {
+    return IMAGE_QUERY_ID;
+  }
+
   public String getSearchDefaultText(boolean isSwedish) {
     return isSwedish ? SIMPLE_SEARCH_DEFAULT_TEXT_SV : SIMPLE_SEARCH_DEFAULT_TEXT_EN;
   }
@@ -219,17 +388,79 @@ public class CommonText {
   public String getChartYAxisLabel(boolean isSwedish) {
     return isSwedish ? CHART_YAXIS_SV : CHART_YAXIS_EN;
   }
+
+  public String getTotal(boolean isSwedish) {
+    return isSwedish ? TOTAL_SV : TOTAL_EN;
+  }
+
+  public String getErrorReportTitle(boolean isSwedish) {
+    return isSwedish ? ERROR_REPORT_TITLE_SV : ERROR_REPORT_TITLE_EN;
+  }
+
+  public String getRegNo(boolean isSweden) {
+    return isSweden ? REG_NO_SV : REG_NO_EN;
+  }
+
+  public String getSpeciesName(boolean isSwedish) {
+    return isSwedish ? SPECIES_NAME_SV : SPECIES_NAME_EN;
+  }
+
+  public String getTypeInformation(boolean isSwedish) {
+    return isSwedish ? TYPE_INFORMATION_SV : TYPE_INFORMATION_EN;
+  }
+
+  public String getFamily(boolean isSwedish) {
+    return isSwedish ? FAMILY_SV : FAMILY_EN;
+  }
+
+  public String getCollectors(boolean isSwedish) {
+    return isSwedish ? COLLECTORS_SV : COLLECTORS_EN;
+  }
+
+  public String getDate(boolean isSwedish) {
+    return isSwedish ? DATE_SV : DATE_EN;
+  }
+
+  public String getLocality(boolean isSwedish) {
+    return isSwedish ? LOCALITY_SV : LOCALITY_EN;
+  }
+
+  public String getCoordinate(boolean isSwedish) {
+    return isSwedish ? COORDINATE_SV : COORDINATE_EN;
+  }
+
+  public String getContinent(boolean isSwedish) {
+    return isSwedish ? CONTINENT_SV : CONTINENT_EN;
+  }
+
+  public String getCountry(boolean isSwedish) {
+    return isSwedish ? COUNTRY_SV : COUNTRY_EN;
+  }
+
+  public String getOtherInformation(boolean isSwedish) {
+    return isSwedish ? OTHER_INFORMATION_SV : OTHER_INFORMATION_EN;
+  }
+
+  public String getDeterminer(boolean isSwedish) {
+    return isSwedish ? DETERMINER_SV : DETERMINER_EN;
+  }
+
+  public String getDescription(boolean isSwedish) {
+    return isSwedish ? DESCRIPTION_SV : DESCRIPTION_EN;
+  }
+
+  public String getReportBy(boolean isSwedish) {
+    return isSwedish ? REPORT_BY_SV : REPORT_BY_EN;
+  }
+
+  public String getHits(boolean isSwedish) {
+    return isSwedish ? HITS_SV : HITS_EN;
+  }
+  
+  public String getSelected(boolean isSwedish) {
+    return isSwedish ? SELECTED_SV : SELECTED_EN;
+  }
 }
-
-
-
-
-
-
-
-
-
-
 
 //  private final static String JAN = "Jan";
 //  private final static String FEB = "Feb";
@@ -243,7 +474,6 @@ public class CommonText {
 //  private final static String OCT = "Oct";
 //  private final static String NOV = "Nov";
 //  private final static String DEC = "Dec";
-
 //  private final static String JAN_SHORT = "Jan";
 //  private final static String FEB_SHORT = "Feb";
 //  private final static String MAR_SHORT = "Mar";
@@ -269,7 +499,6 @@ public class CommonText {
 //  private final static String OCT_SV_SHORT = "okt";
 //  private final static String NOV_SV_SHORT = "nov";
 //  private final static String DEC_SV_SHORT = "dec";
-
 //  private String languageMonthMatch(String mon, boolean isSwedish) {
 //
 //    switch (mon) {
