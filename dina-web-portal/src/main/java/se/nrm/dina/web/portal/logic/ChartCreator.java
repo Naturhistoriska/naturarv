e82f5dd3776d1273d20b01a0c1e353d5f3f4627a
Ida Li <ida.li@nrm.se>
2019-10-24 14:45:35 +0200

    Move out ChartCreator from ChartView

---
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/logic/ChartCreator.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.nrm.dina.web.portal.logic;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Map;
+import java.util.stream.IntStream; 
+import lombok.extern.slf4j.Slf4j;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import se.nrm.dina.web.portal.logic.utils.ChartHelper;
+import se.nrm.dina.web.portal.utils.CommonText;
+
+/**
+ *
+ * @author idali
+ */
+@Slf4j
+public class ChartCreator implements Serializable {
+  
+  /**
+   * Create a month chart
+   * @param model - BarChartModel
+   * @param dataMap - Map<String, Integer>
+   * @param startDate - LocalDateTime
+   * @param isSwedish - boolean
+   */
+  public void createMonthChart(BarChartModel model, Map<String, Integer> dataMap, 
+          LocalDateTime startDate, boolean isSwedish) { 
+    ChartSeries series = new ChartSeries();
+    IntStream.range(0, 12)
+            .forEach(x -> {
+              Month month = startDate.plusMonths(x).getMonth(); 
+              series.set(ChartHelper.getInstance().buildSeriesLabel(month, startDate.getYear(), isSwedish),
+                      dataMap.get(month.name()) == null ? 0 : dataMap.get(month.name()));
+            });
+
+    model.addSeries(series);
+    model.setTitle(CommonText.getInstance().getMonthChartTitle(isSwedish));
+    ChartHelper.getInstance().addOptions(model, true, isSwedish);
+  }
+   
+  /**
+   * Create a year chart
+   * @param model - BarChartModel
+   * @param dataMap - Map<String, Integer>
+   * @param isSwedish - boolean
+   */
+  public void createYearChart(BarChartModel model, Map<String, Integer> dataMap, boolean isSwedish) {
+    ChartSeries series = new ChartSeries();
+    dataMap.entrySet().stream().forEach(e -> {
+      series.set(e.getKey(), e.getValue());
+    });
+
+    model.addSeries(series);
+    model.setTitle(CommonText.getInstance().getYearChartTitle(isSwedish));
+    ChartHelper.getInstance().addOptions(model, false, isSwedish);
+  }
+}
