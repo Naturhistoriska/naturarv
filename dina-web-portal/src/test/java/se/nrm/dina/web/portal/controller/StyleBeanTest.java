31868a163edfd42315b31d9307fcd8e1b51cb015
Ida Li <ida.li@nrm.se>
2019-12-13 05:56:52 -0800

    Add unit test

--- dina-web-portal/src/test/java/se/nrm/dina/web/portal/controller/StyleBeanTest.java
+++ dina-web-portal/src/test/java/se/nrm/dina/web/portal/controller/StyleBeanTest.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.nrm.dina.web.portal.controller;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.After; 
+import org.junit.Before; 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,147 +11,183 @@ import static org.junit.Assert.*;
  */
 public class StyleBeanTest {
   
+  private StyleBean instance;
+   
+  private final String ACTIVE_LINK = "activelink";
+  private final String INACTIVE_LINK = "inactivelink";
+  private final String INACTIVE_TAB_LINK = "inactiveTablink";
+
+  private final String HOME = "home";
+  private final String COLLECTIONS = "collections";
+  private final String PARTNERS = "partners";
+  private final String FAQ = "faq";
+  private final String ABOUT = "about";
+  private final String CONTACT = "contact";
+
+  
   public StyleBeanTest() {
   }
-  
-  @BeforeClass
-  public static void setUpClass() {
-  }
-  
-  @AfterClass
-  public static void tearDownClass() {
-  }
-  
+   
   @Before
   public void setUp() {
+    instance = new StyleBean();
   }
   
   @After
   public void tearDown() {
+    instance = null;
   }
 
   /**
    * Test of getEnLink method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetEnLink() {
-    System.out.println("getEnLink");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getEnLink"); 
+      
     String result = instance.getEnLink();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(INACTIVE_LINK, result); 
   }
 
   /**
    * Test of getSvLink method, of class StyleBean.
    */
-//  @Test
+ @Test
   public void testGetSvLink() {
-    System.out.println("getSvLink");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getSvLink");  
     String result = instance.getSvLink();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(ACTIVE_LINK, result); 
   }
 
   /**
    * Test of getTabStart method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetTabStart() {
-    System.out.println("getTabStart");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getTabStart");  
     String result = instance.getTabStart();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(ACTIVE_LINK, result); 
   }
 
   /**
    * Test of getTabCollections method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetTabCollections() {
-    System.out.println("getTabCollections");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getTabCollections");  
     String result = instance.getTabCollections();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(INACTIVE_TAB_LINK, result); 
   }
 
   /**
    * Test of getTabPartners method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetTabPartners() {
-    System.out.println("getTabPartners");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
-    String result = instance.getTabPartners();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    System.out.println("getTabPartners");  
+    String result = instance.getTabPartners(); 
+    assertEquals(INACTIVE_TAB_LINK, result); 
   }
 
   /**
    * Test of getTabFaq method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetTabFaq() {
-    System.out.println("getTabFaq");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getTabFaq");  
     String result = instance.getTabFaq();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(INACTIVE_TAB_LINK, result); 
   }
 
   /**
    * Test of getTabAbout method, of class StyleBean.
    */
-//  @Test
+  @Test
   public void testGetTabAbout() {
-    System.out.println("getTabAbout");
-    StyleBean instance = new StyleBean();
-    String expResult = "";
+    System.out.println("getTabAbout");  
     String result = instance.getTabAbout();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(INACTIVE_TAB_LINK, result); 
   }
 
   /**
    * Test of setTabStyle method, of class StyleBean.
    */
-//  @Test
-  public void testSetTabStyle() {
+  @Test
+  public void testSetTabStyle1() {
+    System.out.println("setTabStyle"); 
+    
+    instance.setTabStyle(HOME);   
+    assertEquals(ACTIVE_LINK, instance.getTabStart()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabAbout()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabCollections()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabFaq()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabPartners());  
+  }
+  
+    @Test
+  public void testSetTabStyle2() {
     System.out.println("setTabStyle");
-    String tab = "";
-    StyleBean instance = new StyleBean();
-    instance.setTabStyle(tab);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    instance.setTabStyle(ABOUT);   
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabStart()); 
+    assertEquals(ACTIVE_LINK, instance.getTabAbout()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabCollections()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabFaq()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabPartners());  
+  }
+  
+    @Test
+  public void testSetTabStyle3() {
+    System.out.println("setTabStyle");
+ 
+    instance.setTabStyle(COLLECTIONS); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabStart()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabAbout()); 
+    assertEquals(ACTIVE_LINK, instance.getTabCollections()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabFaq()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabPartners());  
+  }
+  
+    @Test
+  public void testSetTabStyle4() {
+    System.out.println("setTabStyle"); 
+    instance.setTabStyle(FAQ); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabStart()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabAbout()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabCollections()); 
+    assertEquals(ACTIVE_LINK, instance.getTabFaq()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabPartners());  
+  }
+  
+  
+  @Test
+  public void testSetTabStyle5() {
+    System.out.println("setTabStyle"); 
+    instance.setTabStyle(PARTNERS); 
+    
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabStart()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabAbout()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabCollections()); 
+    assertEquals(INACTIVE_TAB_LINK, instance.getTabFaq()); 
+    assertEquals(ACTIVE_LINK, instance.getTabPartners());  
   }
 
   /**
    * Test of setLanguageStyle method, of class StyleBean.
    */
-//  @Test
-  public void testSetLanguageStyle() {
-    System.out.println("setLanguageStyle");
-    String locale = "";
-    StyleBean instance = new StyleBean();
-    instance.setLanguageStyle(locale);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+  @Test
+  public void testSetLanguageStyleSv() { 
+    String locale = "sv"; 
+    instance.setLanguageStyle(locale); 
+    assertEquals(instance.getSvLink(), ACTIVE_LINK);
+    assertEquals(instance.getEnLink(), INACTIVE_LINK);
+  }
+  
+  @Test
+  public void testSetLanguageStyleEn() { 
+    String locale = "en"; 
+    instance.setLanguageStyle(locale); 
+    assertEquals(instance.getSvLink(), INACTIVE_LINK);
+    assertEquals(instance.getEnLink(), ACTIVE_LINK);
   }
   
 }
