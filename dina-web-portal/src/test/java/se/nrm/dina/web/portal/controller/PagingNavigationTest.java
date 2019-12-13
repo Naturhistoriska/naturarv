5714d583dec624270b688eb0c34b31b7deaada1a
Ida Li <ida.li@nrm.se>
2019-12-13 03:50:14 -0800

    Add unit test

--- dina-web-portal/src/test/java/se/nrm/dina/web/portal/controller/PagingNavigationTest.java
+++ dina-web-portal/src/test/java/se/nrm/dina/web/portal/controller/PagingNavigationTest.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.nrm.dina.web.portal.controller;
 
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.After; 
+import org.junit.Before; 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -19,19 +12,14 @@ import static org.junit.Assert.*;
  */
 public class PagingNavigationTest {
   
+  private PagingNavigation instance;
+  
   public PagingNavigationTest() {
   }
-  
-  @BeforeClass
-  public static void setUpClass() {
-  }
-  
-  @AfterClass
-  public static void tearDownClass() {
-  }
-  
+ 
   @Before
   public void setUp() {
+    instance = new PagingNavigation();
   }
   
   @After
@@ -41,166 +29,265 @@ public class PagingNavigationTest {
   /**
    * Test of calculateTotalPages method, of class PagingNavigation.
    */
-//  @Test
-  public void testCalculateTotalPages() {
+  @Test
+  public void testCalculateTotalPages1() {
     System.out.println("calculateTotalPages");
-    int totalFound = 0;
-    int numPerPage = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.calculateTotalPages(totalFound, numPerPage);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    int totalFound = 288;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    List pages = instance.getPages();
+    int pageSize = pages.size();
+    
+    assertEquals(instance.getTotalPages(), 29); 
+    assertEquals(instance.getEnd(), 10); 
+    assertEquals(pageSize, 10);
+    assertEquals(pages.get(0), 1);
+    assertEquals(pages.get(pageSize - 1), 10);
+  }
+  
+  @Test
+  public void testCalculateTotalPages2() {
+    System.out.println("calculateTotalPages");
+    int totalFound = 16;
+    int numPerPage = 25; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    assertEquals(instance.getTotalPages(), 1);
+    assertEquals(instance.getEnd(), 25);
+    assertEquals(instance.getPages().size(), 1);
+  }
+  
+  @Test
+  public void testCalculateTotalPages3() {
+    System.out.println("calculateTotalPages");
+    int totalFound = 88;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    List pages = instance.getPages();
+    int pageSize = pages.size();
+    
+    assertEquals(instance.getTotalPages(), 9);
+    assertEquals(instance.getEnd(), 10);
+    assertEquals(pageSize, 9);
+    assertEquals(pages.get(0), 1);
+    assertEquals(pages.get(pageSize - 1), 9);
   }
 
   /**
    * Test of setNextPage method, of class PagingNavigation.
    */
-//  @Test
-  public void testSetNextPage() {
-    System.out.println("setNextPage");
-    int numPerPage = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.setNextPage(numPerPage);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+  @Test
+  public void testSetNextPage1() {
+    System.out.println("se  NextPage");
+    int numPerPage = 10; 
+    int totalFound = 288; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    instance.setNextPage(numPerPage); 
+     
+    assertEquals(instance.getStart(), 11);
+    assertEquals(2, instance.getCurrentPage()); 
+    assertEquals(instance.getEnd(), 20); 
+  }
+  
+  @Test
+  public void testSetNextPage2() {
+    System.out.println("se  NextPage");
+    int numPerPage = 10; 
+    int totalFound = 18; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    instance.setNextPage(numPerPage); 
+    assertEquals(instance.getStart(), 11);
+    assertEquals(2, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 18);
   }
 
   /**
    * Test of setPreviousPage method, of class PagingNavigation.
    */
-//  @Test
-  public void testSetPreviousPage() {
+  @Test
+  public void testSetPreviousPage1() {
     System.out.println("setPreviousPage");
-    int numPerPage = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.setPreviousPage(numPerPage);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+   
+    int totalFound = 88;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    instance.setNextPage(numPerPage);
+    instance.setNextPage(numPerPage);
+    
+    instance.setPreviousPage(numPerPage); 
+    
+    assertEquals(11, instance.getStart());
+    assertEquals(2, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 20);
   }
 
+  @Test
+  public void testSetPreviousPage2() {
+    System.out.println("setPreviousPage");
+   
+    int numPerPage = 10; 
+    int totalFound = 28; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    instance.setNextPage(numPerPage);
+    instance.setNextPage(numPerPage);
+    
+    instance.setPreviousPage(numPerPage); 
+    
+    assertEquals(11, instance.getStart());
+    assertEquals(2, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 20);
+  }
+  
   /**
    * Test of setFirstPage method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testSetFirstPage() {
     System.out.println("setFirstPage");
-    int numPerPage = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.setFirstPage(numPerPage);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    int numPerPage = 10; 
+    int totalFound = 28; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    instance.setFirstPage(numPerPage); 
+     
+    assertEquals(1, instance.getStart());
+    assertEquals(1, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 10);
   }
 
   /**
    * Test of setLastPage method, of class PagingNavigation.
    */
-//  @Test
-  public void testSetLastPage() {
+  @Test
+  public void testSetLastPage1() {
     System.out.println("setLastPage");
-    int numDisplay = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.setLastPage(numDisplay);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    int totalFound = 288;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    instance.setLastPage(numPerPage); 
+    assertEquals(281, instance.getStart());
+    assertEquals(29, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 288);
+    
+  }
+  
+  @Test
+  public void testSetLastPage2() {
+    System.out.println("setLastPage");
+    int totalFound = 89;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    instance.setLastPage(numPerPage); 
+    assertEquals(81, instance.getStart());
+    assertEquals(9, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 89); 
   }
 
   /**
    * Test of setPaging method, of class PagingNavigation.
    */
-//  @Test
-  public void testSetPaging() {
+  @Test
+  public void testSetPaging1() {
     System.out.println("setPaging");
-    int start = 0;
-    int numPerPage = 0;
-    int currentPage = 0;
-    PagingNavigation instance = new PagingNavigation();
-    instance.setPaging(start, numPerPage, currentPage);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    
+    int totalFound = 288;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+     
+    int start = 50; 
+    int currentPage = 5; 
+    instance.setPaging(start, numPerPage, currentPage); 
+    assertEquals(51, instance.getStart());
+    assertEquals(5, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 60);
+    assertEquals(instance.getPages().size(), 10);
+  }
+  
+  @Test
+  public void testSetPaging2() {
+    System.out.println("setPaging");
+    
+    int totalFound = 66;
+    int numPerPage = 10; 
+    instance.calculateTotalPages(totalFound, numPerPage); 
+    
+    
+    int start = 50; 
+    int currentPage = 5; 
+    instance.setPaging(start, numPerPage, currentPage); 
+    assertEquals(51, instance.getStart());
+    assertEquals(5, instance.getCurrentPage());
+    assertEquals(instance.getEnd(), 60);
+    assertEquals(instance.getPages().size(), 7);
   }
 
   /**
    * Test of getStart method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetStart() {
-    System.out.println("getStart");
-    PagingNavigation instance = new PagingNavigation();
+    System.out.println("getStart"); 
     int expResult = 0;
     int result = instance.getStart();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(expResult, result); 
   }
 
   /**
    * Test of getEnd method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetEnd() {
-    System.out.println("getEnd");
-    PagingNavigation instance = new PagingNavigation();
+    System.out.println("getEnd"); 
     int expResult = 0;
     int result = instance.getEnd();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(expResult, result); 
   }
 
   /**
    * Test of getTotalPages method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetTotalPages() {
-    System.out.println("getTotalPages");
-    PagingNavigation instance = new PagingNavigation();
-    int expResult = 0;
+    System.out.println("getTotalPages"); 
+    int expResult = 1;
     int result = instance.getTotalPages();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(expResult, result); 
   }
 
   /**
    * Test of getTotalFound method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetTotalFound() {
-    System.out.println("getTotalFound");
-    PagingNavigation instance = new PagingNavigation();
+    System.out.println("getTotalFound"); 
     int expResult = 0;
     int result = instance.getTotalFound();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(expResult, result); 
   }
 
   /**
    * Test of getPages method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetPages() {
-    System.out.println("getPages");
-    PagingNavigation instance = new PagingNavigation();
-    List<Integer> expResult = null;
+    System.out.println("getPages");  
     List<Integer> result = instance.getPages();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertNotNull(result); 
   }
 
   /**
    * Test of getCurrentPage method, of class PagingNavigation.
    */
-//  @Test
+  @Test
   public void testGetCurrentPage() {
-    System.out.println("getCurrentPage");
-    PagingNavigation instance = new PagingNavigation();
-    int expResult = 0;
+    System.out.println("getCurrentPage"); 
+    int expResult = 1;
     int result = instance.getCurrentPage();
-    assertEquals(expResult, result);
-    // TODO review the generated test code and remove the default call to fail.
-    fail("The test case is a prototype.");
+    assertEquals(expResult, result); 
   }
   
 }
