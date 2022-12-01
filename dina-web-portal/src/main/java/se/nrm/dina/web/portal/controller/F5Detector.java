6af7aa8674c2ac5d231df14a90c484b38f920552
Ida Li <ida.li@nrm.se>
2022-12-01 10:24:32 +0100

    Add for collection query

---
+++ dina-web-portal/src/main/java/se/nrm/dina/web/portal/controller/F5Detector.java
@@ -0,0 +1,76 @@
+package se.nrm.dina.web.portal.controller;
+
+import java.io.Serializable; 
+import javax.enterprise.context.SessionScoped; 
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+//import se.nrm.dina.web.portal.utils.CommonText; 
+
+/**
+ *
+ * @author idali
+ */
+@SessionScoped
+@Named("f5Detector")
+@Slf4j
+public class F5Detector implements Serializable {
+ 
+    private final String qryCollection = "collection";
+    private boolean isCollectionSearch;
+    private final String pb = "pb";
+    private final String qryDataset = "dataset";
+    
+//    private final String baseUrl = "https://naturarv.nrm.se/";
+    private final String baseUrl = "http://localhost:8180/";
+    private final String collectionUri = "/faces/pages/collectionresults.xhtml";
+    private final String resultUri = "/faces/pages/results.xhtml";
+    
+    private final String slash = "/";
+//    private List<String> updateList; 
+    
+//    private HttpSession session;  
+
+    @Inject
+    private SearchBean search;
+
+    public void checkF5() {
+        log.info("checkF5");
+
+        isCollectionSearch = false;
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequest();
+         
+        String url = request.getRequestURL().toString();
+        String uri = request.getRequestURI(); 
+         
+        
+        log.info("url and uri : {} -- {}", url, uri);
+        log.info("pathInfo : {}", request.getQueryString());
+        String queryString = request.getQueryString();
+        String collection = request.getParameter(qryCollection);  
+        String dataset = null;
+
+        if (uri == null || uri.equals(slash)) {
+            if (!StringUtils.isBlank(collection)) {
+                if (collection.equals(pb)) {
+                    dataset = request.getParameter(qryDataset);
+                }
+                search.searchCollectionWithQuery(collection, dataset, queryString);
+                isCollectionSearch = true;
+            }
+        } else if(uri.equals(collectionUri)) {
+            isCollectionSearch = true;
+        }  
+        log.info("url : {} -- {}", collection, isCollectionSearch); 
+    }
+
+    public boolean isIsCollectionSearch() {
+        return isCollectionSearch;
+    }
+
+}
